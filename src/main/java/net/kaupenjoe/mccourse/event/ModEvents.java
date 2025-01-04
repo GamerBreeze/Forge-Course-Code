a90b5f81b6548c1d796bfcc01e4d3fa6c7db4c69
GamerBreeze <nordqvistville@gmail.com>
2025-01-04 20:56:11 +0100

    Super Important!

---
+++ src/main/java/net/kaupenjoe/mccourse/event/ModEvents.java
@@ -0,0 +1,43 @@
+package net.kaupenjoe.mccourse.event;
+
+import net.kaupenjoe.mccourse.MCCourseMod;
+import net.kaupenjoe.mccourse.item.custom.HammerItem;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.event.level.BlockEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Mod.EventBusSubscriber(modid = MCCourseMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class ModEvents {
+    private static final Set<BlockPos> HARVESTED_BLOCKS = new HashSet<>();
+
+    @SubscribeEvent
+    public static void onHammerUsage(BlockEvent.BreakEvent event) {
+        Player player = event.getPlayer();
+        ItemStack mainHandItem = player.getMainHandItem();
+
+        if(mainHandItem.getItem() instanceof HammerItem hammerItem && player instanceof ServerPlayer serverPlayer) {
+            BlockPos initialBlockPos = event.getPos();
+            if(HARVESTED_BLOCKS.contains(initialBlockPos)) {
+                return;
+            }
+
+            for(BlockPos pos : HammerItem.getBlocksToBeDestroyed(1, initialBlockPos, serverPlayer)) {
+                if(pos == initialBlockPos || !hammerItem.isCorrectToolForDrops(mainHandItem, event.getLevel().getBlockState(pos))) {
+                    continue;
+                }
+
+                HARVESTED_BLOCKS.add(pos);
+                serverPlayer.gameMode.destroyBlock(pos);
+                HARVESTED_BLOCKS.remove(pos);
+
+            }
+        }
+    }
+}
